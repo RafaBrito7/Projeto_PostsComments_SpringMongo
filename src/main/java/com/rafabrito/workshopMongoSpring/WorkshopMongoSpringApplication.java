3a78a8c4ea354ec48fefcc66b6c0671f828a997a
RafaBrito7 <rafabrito7@gmail.com>
2020-07-11 11:48:28 -0300

    Projeto Criado

---
+++ src/main/java/com/rafabrito/workshopMongoSpring/WorkshopMongoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.rafabrito.workshopMongoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMongoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopMongoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafabrito/workshopMongoSpring/WorkshopMongoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafabrito.workshopMongoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
