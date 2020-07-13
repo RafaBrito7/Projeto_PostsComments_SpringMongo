2f6736f961fe6090811334e5806e21f3dabfee72
RafaBrito7 <rafabrito7@gmail.com>
2020-07-13 11:56:15 -0300

    Consulta Simples com o @Querry

--- src/main/java/com/rafabrito/workshopMongoSpring/repository/PostRepository.java
+++ src/main/java/com/rafabrito/workshopMongoSpring/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.rafabrito.workshopMongoSpring.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.rafabrito.workshopMongoSpring.domain.Post;
@@ -10,6 +11,10 @@ import com.rafabrito.workshopMongoSpring.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	//O Próprio MongoRepository, já contei todos os métodos necessários para utilização, não precisamos criar nenhum...
+	
+	// Estrutura utilizada: { <field>: { $regex: /pattern/, $options: '<options>' } }
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	// Para implementar consultas com Querry Methods do próprio Spring
 	List<Post> findByTitleContainingIgnoreCase (String text);
