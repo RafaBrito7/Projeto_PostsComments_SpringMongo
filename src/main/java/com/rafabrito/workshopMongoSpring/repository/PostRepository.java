9c31ed1472097ef9b3900eaa9df8e0f7e7b3c45d
RafaBrito7 <rafabrito7@gmail.com>
2020-07-13 11:30:18 -0300

    Add Search By Title in Posts with QuerryMethods

--- src/main/java/com/rafabrito/workshopMongoSpring/repository/PostRepository.java
+++ src/main/java/com/rafabrito/workshopMongoSpring/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.rafabrito.workshopMongoSpring.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,7 @@ import com.rafabrito.workshopMongoSpring.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	//O Próprio MongoRepository, já contei todos os métodos necessários para utilização, não precisamos criar nenhum...
+	
+	// Para implementar consultas com Querry Methods do próprio Spring
+	List<Post> findByTitleContainingIgnoreCase (String text);
 	}
--- src/main/java/com/rafabrito/workshopMongoSpring/resources/PostResources.java
+++ src/main/java/com/rafabrito/workshopMongoSpring/resources/PostResources.java
@@ -1,13 +1,17 @@
 package com.rafabrito.workshopMongoSpring.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rafabrito.workshopMongoSpring.domain.Post;
+import com.rafabrito.workshopMongoSpring.resources.util.URL;
 import com.rafabrito.workshopMongoSpring.services.PostServices;
 
 @RestController // Anunciando que a classe vai ser um recurso Rest
@@ -22,6 +26,13 @@ public class PostResources {
 	public ResponseEntity<Post> findById(@PathVariable String id){ // O Path sinalizará que o ID do argumento vai casar com o id da URL
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){ // O request formatará a url no padrão http titlesearch?text=parametromsg
+		text = URL.decodeParam(text); // Para encontrar a formatação http do text, inpencionar elemento no chrome e pesquisar no console o encodeURIComponent("bom dia")
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 	
 }
